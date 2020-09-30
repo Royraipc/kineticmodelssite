b7ac1d4405a0fbb31b8565a4d24ed7cc5cab7a84
Royraipc <71947442+Royraipc@users.noreply.github.com>
2020-10-01 00:54:18 +0530

    update kineticssite

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kineticssite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kineticsdatabase.settings")   #changed kineticsdatabase from kineticssite
 
     from django.core.management import execute_from_command_line
 
